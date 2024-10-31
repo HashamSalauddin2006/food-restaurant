4d1d2b3ec43e08a36d4d2810fc30e006e66a6c4f
HashamSalauddin2006 <hashamsalauddin2006@gmail.com>
2024-11-01 02:09:22 +0500

    Done

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-//active navber
-let nav = document.querySelecto(".navitagion-wrap");
-window.onscroll = function () {
-    if(document.documentElement.scrollTop  > 20){
-        nav.classList.add("scroll-on");
-    }else{
-        nav.classList.remove("scroll-on");
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+      let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+          current += increment;
+          obj.textContent = current;
+          if (current == end) {
+            clearInterval(timer);
+          }
+        }, step);
     }
-
-}   
-//nav hide:
-let navBar = document.querySelectorAll('.nav-link');
-let navcollapse = document.querySelector('navbar-collopse.collopse');
-navBar.forEach(function(a)){
-    a.addEventListener("click",function()){
-        navcollapse.classList.remove("show");
-    }
-}
+  
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+  });
+  
